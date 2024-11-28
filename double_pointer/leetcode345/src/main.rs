6207dc6567ba2012e3569bbae6a22341cd2ac9e9
haydn <haydn.kong@outlook.com>
2024-11-28 05:48:05 +0000

    Modfied leetcode345

--- double_pointer/leetcode345/src/main.rs
+++ double_pointer/leetcode345/src/main.rs
@@ -5,8 +5,8 @@ const VOWELS: [u8; 10] = [b'A', b'E', b'I', b'O', b'U', b'a', b'e', b'i', b'o', 
 impl Solution {
     pub fn reverse_vowels(s: String) -> String {
         let mut s = s.into_bytes();
-        let mut left: isize = 0;
-        let mut right:isize = (s.len() - 1).try_into().unwrap();
+        let mut left = 0;
+        let mut right  = s.len() - 1;
 
         while left < right {
             // 在此处创建变量，简化后续判断的表达式，但变量只会在外层while的每次循环中被赋值一次。
@@ -21,21 +21,23 @@ impl Solution {
             //     right -= 1;
             // }
 
-            while !VOWELS.contains(&s[left as usize]) && (left < right) {
-                // println!("left = {}", left);
+            while (left < right) && !VOWELS.contains(&s[left]) {
                 left += 1;
             }
 
-            while !VOWELS.contains(&s[right as usize]) && (left < right) {
-                // println!("right = {}", right);
+            while (left < right) && !VOWELS.contains(&s[right]) {
                 right -= 1;
             }
 
             // Vec<>的index类型是usize，如果left = 0, right = 0
             // right -= 1后会导致overflow
-            s.swap(left as usize, right as usize);
-            left += 1;
-            right -= 1;
+            // 一种方式是将left、right声明为isize，作为index时as uszie
+            // 另一种方式是在此处先判断一下
+            if left < right {
+                s.swap(left, right);
+                left += 1;
+                right -= 1;
+            }
         }
         String::from_utf8(s).unwrap()
     }
