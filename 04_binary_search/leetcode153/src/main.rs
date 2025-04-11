d25f48517cc5087b37f10acfd0eeb0eebdbf9ab5
ethan1652029449 <149357197+ethan1652029449@users.noreply.github.com>
2025-04-11 11:01:21 +0800

    Added leetcode34

--- 04_binary_search/leetcode153/src/main.rs
+++ 04_binary_search/leetcode153/src/main.rs
@@ -1,11 +1,37 @@
 pub struct Solution {}
 
+// 			        _
+// 		        _
+// 	        _
+//      _
+// 						        _
+// 					        _
+// 				        _
+
+// 一个升序数组经过旋转后，会变成上图所示，
+// 我们应该想办法找到右边线段的最左侧。
+// 如果mid落在左边线段上，就有:
+// nums[mid] ≥ nums[left] 大于等于nums[right]
+// 此时应该把搜索范围控制在mid+1,right之间。
+// （已经知道mid在左边，因此是从mid+1开始）。
+
+// 如果循环条件是left <= right：
+// 倒数第二次遍历时，left和mid是左边最高点。
+// right是右边最低点。此时进入if分支，
+// left = mid + 1后 = right。
+// 此刻还能最后遍历一次，
+// 最终left = right = mid = 最低点。
+
+// 根据上面的分析，倒数第二次遍历时，
+// right就已经等于最低点了。
+// 因此可以把循环条件改成，
+// while left < right。
 impl Solution {
     pub fn find_min(nums: Vec<i32>) -> i32 {
         let mut left = 0;
         let mut right = nums.len() - 1;
 
-        while left <= right {
+        while left < right {
             let mid = left + (right - left) / 2;
             if nums[mid] >= nums[left]
                 && nums[left] >= nums[right]
@@ -20,7 +46,7 @@ impl Solution {
 }
 
 fn main() {
-    let nums = vec![11, 13, 15, 17];
+    let nums = vec![4, 5, 6, 7, 0, 1, 2];
 
     let res = Solution::find_min(nums);
 
---
+++ 04_binary_search/leetcode34/src/main.rs
@@ -0,0 +1,76 @@
+pub struct Solution {}
+
+// 实际上是找到两个target，分别是左右边界。
+// 遇到的问题：
+// 1.找右边界时，如果mid=l+(r-l)/2;
+// nums = [5,7,7,8,8,10], target = 8
+// 上面例子就会无法退出循环。
+// 原因在于l=mid=4时，r=5永远无法向右移动。
+// 2. nums[5,7,7,8,8,10], target = 10,
+// 可能会出现边界溢出的问题，
+// 原因在于在循环中l = m + 1，超出了边界。
+// 解决办法：
+// 1. 使mid=l+(r-l)/2+1,灵感乍现且有效的方法
+// 归结与一种更合适的对称性，
+// 寻找左右边界时逻辑上的对称性。
+// 2. while left<=right改为left<right.
+impl Solution {
+    pub fn search_range(
+        nums: Vec<i32>,
+        target: i32,
+    ) -> Vec<i32> {
+        let mut res = vec![-1, -1];
+        let n = nums.len();
+
+        if n == 0 || nums[0] > target || nums[n - 1] < target {
+            return res;
+        }
+
+        let mut left = 0;
+        let mut right = n - 1;
+
+        // 找到左边界
+        while left < right {
+            let mid = left + (right - left) / 2;
+            if nums[mid] < target {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        // 存储答案
+        if nums[left] == target {
+            res[0] = left as i32;
+        }
+
+        // 格式化
+        left = 0;
+        right = n - 1;
+
+        // 找到右边界
+        while left < right {
+            let mid = left + (right - left) / 2 + 1;
+            if nums[mid] > target {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+
+        // 储存答案
+        if nums[left] == target {
+            res[1] = left as i32;
+        }
+
+        res
+    }
+}
+
+fn main() {
+    let nums = vec![5, 7, 7, 8, 8, 10];
+
+    let res = Solution::search_range(nums, 8);
+
+    println!("rseult: {:?}", res);
+}
